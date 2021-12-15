78742ceba79d33baf92738f962030a8f0f3279be
jgumerove <josh.gumerove@gmail.com>
2021-12-15 14:26:36 -0500

    seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+france = Country.create(name: "France")
+italy = Country.create(name: "Italy")
+germany = Country.create(name: "Germany")
+
+france.wines.create(title: "French Wine", price: 900, wine_category: "White", year: 1991)
+france.wines.create(title: "French Red", price: 100, wine_category: "Red", year: 1988)
+italy.wines.create(title: "Italy Red", price: 110, wine_category: "Red", year: 1978)
+italy.wines.create(title: "Italy Something", price: 88, wine_category: "Pinot", year: 1978)
+germany.wines.create(title: "German Reisling", price: 188, wine_category: "Resiling", year: 1999)
