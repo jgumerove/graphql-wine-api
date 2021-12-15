3251c7419d33e737ff506dc711befe3db69278ad
jgumerove <josh.gumerove@gmail.com>
2021-12-15 14:35:38 -0500

    add descriptions to queries

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,12 +1,18 @@
 module Types
   class QueryType < Types::BaseObject
     
-    field :countries, [Types::CountryType], null: false
+    field :countries, [Types::CountryType], null: false do
+      description "Displays all the countries"
+    end
+    
     def countries
       Country.all
     end
 
-    field :wines, [Types::WineType], null: false
+    field :wines, [Types::WineType], null: false do
+      description "Displays all the wines"
+    end
+
     def wines
       Wine.all
     end
