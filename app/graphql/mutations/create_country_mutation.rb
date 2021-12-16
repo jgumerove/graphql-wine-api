1c4e9567da5364ac9900e1c79b1eb83b75e49814
jgumerove <josh.gumerove@gmail.com>
2021-12-16 12:42:32 -0500

    create country mutation

--- app/graphql/mutations/create_country_mutation.rb
+++ app/graphql/mutations/create_country_mutation.rb
@@ -2,14 +2,21 @@ module Mutations
   class CreateCountryMutation < BaseMutation
     # TODO: define return fields
     field :country, Types::CountryType, null: false
+    field :errors, [String], null: true
     argument :name, String, required: true
+
 
     # TODO: define resolve method
     def resolve(name: name)
-      @country = Country.new(name: name)
-      if(@country.save)
+      country = Country.new(name: name)
+      if country.save
         {
-          country: @country
+          country: country
+        }
+      else
+        {
+          country: nil,
+          errors: country.errors.full_messages
         }
       end
     end
--- app/graphql/types/country_type.rb
+++ app/graphql/types/country_type.rb
@@ -4,7 +4,7 @@ module Types
     field :name, String, null: false
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
-    field :wines, [WineType], null: false
+    field :wines, [WineType], null: true
     field :wine_count, Integer, null: true
     def wine_count
       object.wines.count
