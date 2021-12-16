92bd7ec86135763bc009c2e650c7c9251c755138
jgumerove <josh.gumerove@gmail.com>
2021-12-16 13:11:00 -0500

    create wine mutation file

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,5 +1,6 @@
 module Types
   class MutationType < Types::BaseObject
+    field :create_wine_mutation, mutation: Mutations::CreateWineMutation
     field :create_country_mutation, mutation: Mutations::CreateCountryMutation
   end
 end
